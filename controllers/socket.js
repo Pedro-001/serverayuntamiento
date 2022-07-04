77d0f9ed9d214668bb45e78e8a1c75ce89f99daa
Pedro Alberto Barrios Molina <pedro.a.barriosmolina@gmail.com>
2022-07-03 21:51:44 -0500

    socket cliente conectado desconectado

---
+++ controllers/socket.js
@@ -0,0 +1,32 @@
+const Usuario =  require('../models/usuario');
+
+
+
+const usuarioConectado  = async(uid = '')=>{
+
+    const usuario = await Usuario.findByid(uid);
+    usuario.online =  true;
+
+    await usuario.save();
+
+    return usuario;
+
+
+}
+
+
+const usuarioDesconectado  = async(uid = '')=>{
+
+    const usuario = await Usuario.findByid(uid);
+    usuario.online =  false;
+
+    await usuario.save();
+
+    return usuario;
+
+
+}
+
+module.exports = {
+    usuarioConectado, usuarioDesconectado
+}
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,28 @@
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
-    console.log(client.handshake.headers);
+    console.log(client.handshake.headers['x-token']);
 
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
-    });
-
+    ///    /// comprobar JWT
+        const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+        console.log(valido);
+    
+    ///     Verificar autenticación
+    ///    if(!valido){ return client.disconnect();}
+    ///    console.log('cliente autenticado');
+            usuarioConectado(uid);
+    ///    ///
+    ///
+        client.on('disconnect', () => {
+            usuarioDesconectado(uid);
+            console.log('Cliente desconectado');
+        });
+    ///
 //    client.on('mensaje', ( payload ) => {
 //        console.log('Mensaje', payload);
 //
