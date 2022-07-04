e1e9e4203cb566b22fc1c990c70d360eab40ec10
Pedro Alberto Barrios Molina <pedro.a.barriosmolina@gmail.com>
2022-07-03 22:44:33 -0500

    socket cliente conectado desconectado correcion 4

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,12 +1,14 @@
 const { response } = require("express");
+const Usuario = require('../models/usuario');
 
+const getUsuarios = async (req, res = response )=> {
 
-const getUsuarios = (req, res = response )=> {
+    const usuarios = await  Usuario.find();
 
     //{ok: true, msg: 'getUsuarios}
     res.json({
         ok:true,
-        msg: 'getUsuarios'
+        usuarios
     })
 
 
